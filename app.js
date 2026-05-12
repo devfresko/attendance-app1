d5b3486f5382ae41c61605855ef74516a2ca61ca
FRESKO <mis@fresko.co.in>
2026-05-12 08:43:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ══════════════════════════════════════════════════════════
 // AttendanceApp — app.js  (Premium Build)
 // ══════════════════════════════════════════════════════════
-var API = 'https://script.google.com/macros/s/AKfycbw0lYuYWeT3_08skqxrREXImPN5B-NMZ8tl0A8_2BPNSOVxOOaCIeUgJBxyTkphdmOOcQ/exec';
+var API = 'https://script.google.com/macros/s/AKfycbyJKUwX_XZawuX88HGsa1x_RU6aq903LcgRxTpfwv-an_8cs9uIbrj3F1-Idp9tqXArag/exec';
 
 // ── State ────────────────────────────────────────────────
 var _U = null, _TOKEN = null, _cbIdx = 0;
